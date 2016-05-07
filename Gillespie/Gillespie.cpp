fac0d29ff0b4389b6a1d5933fe611d45b35974a4
kosyumote <kosyumote@gmail.com>
2016-05-07 17:58:19 -0400

    Create Gillespie.cpp
    This is a Gillespie model simulator, that takes arbitrary numbers of variables and equations, specified by an input file. (When I started programming it, I didn't do the file approach; will be fixed in next commit.) The simulation should be as fast as possible, but does not parallelize, as most threads would be waiting most of the time anyway (in fact, the synchronization of threads may take longer than the speedup you might get from parallelization). It will use two arrays, one to store the number of instances of each variable (1D array of length var), and one to store the equations and their rates (2D array of size equations * (var + 1)).

---
+++ Gillespie/Gillespie.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+#define NUMBER_OF_VARIABLES 6
+#define MAX_TIME 1
+#define NUMBER_OF_EQUATIONS 8
+
+double rates(int* vars, int number)
+{
+  double rate = 0;
+  switch(number)
+  {
+  case 0:
+ {
+ 
+ }
+case 1:
+{
+
+}
+case 2:
+{
+
+}
+case 3:
+{
+
+}
+case 4:
+{
+
+}
+case 5:
+{
+
+}
+case 6:
+{
+
+}
+case 7:
+{
+
+}
+case 8:
+{
+
+}
+}
+  return rate;
+}
+
+int main()
+{
+  int* vars = new int[NUMBER_OF_VARIABLES];
+  
+  
+
+  delete[] vars;
+}
